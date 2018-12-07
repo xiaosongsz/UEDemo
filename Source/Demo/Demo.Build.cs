44d06258c941cfc77e88073e26f1b4e8a9f06a2b
杨晓松 <xiaosongsz@foxmail.com>
2018-12-07 13:16:26 +0800

    删除slua插件

--- Source/Demo/Demo.Build.cs
+++ Source/Demo/Demo.Build.cs
@@ -14,11 +14,6 @@ public class Demo : ModuleRules
 
         PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-        PrivateDependencyModuleNames.AddRange(new string[] { "slua_unreal", "Slate", "SlateCore", "UMG" });
-
-        PrivateIncludePathModuleNames.AddRange(new string[] { "slua_unreal" });
-        PublicIncludePathModuleNames.AddRange(new string[] { "slua_unreal" });
-
         // Uncomment if you are using Slate UI
         // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
